6f3682ccf54cf1e2b8976a9cb79d5aea1ad8abea
amir tadros <amirtadros01@gmail.com>
2024-03-08 12:04:55 -0800

    add potential stages and stage switch block

--- SmartCycle/src/main.cpp
+++ SmartCycle/src/main.cpp
@@ -4,9 +4,17 @@ constexpr auto REED_SWITCH_PIN{13};
 constexpr auto SHIFT_UP_BUTTON_PIN{12};
 constexpr auto SHIFT_DOWN_BUTTON_PIN{27};
 
+enum class States {
+    Sleeping,
+    Waiting,
+    Coasting,
+    Shifting
+} current_state;
+
 void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  switch(current_state) {
+  }
 }
