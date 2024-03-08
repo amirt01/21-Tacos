c329178fed9dc8ff8fafe34870fcece23c48fcc5
amir tadros <amirtadros01@gmail.com>
2024-03-08 12:30:23 -0800

    add sensor flags and define ISRs to set corresponding sensor flag

--- SmartCycle/src/main.cpp
+++ SmartCycle/src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+#include <bitset>
+
 static constexpr auto REED_SWITCH_PIN{13};
 static constexpr auto SHIFT_UP_BUTTON_PIN{12};
 static constexpr auto SHIFT_DOWN_BUTTON_PIN{27};
@@ -14,6 +16,14 @@ enum class States {
 void reed_switch_isr();
 void shift_up_button_isr();
 void shift_down_button_isr();
+
+enum sensor_flag_ids {
+  reed_switch,
+  shift_up_button,
+  shift_down_button,
+  n_sensor_flags
+};
+std::bitset<n_sensor_flags> sensor_flags{};
 
 void setup() {
   Serial.begin(115200);
@@ -30,4 +40,17 @@ void setup() {
 void loop() {
   switch(current_state) {
   }
+}
+
+void reed_switch_isr() {
+  sensor_flags.set(sensor_flag_ids::reed_switch);
+}
+
+void shift_up_button_isr() {
+  sensor_flags.set(sensor_flag_ids::shift_up_button);
+  
+}
+
+void shift_down_button_isr() {
+  sensor_flags.set(sensor_flag_ids::shift_down_button);
 }
