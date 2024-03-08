b1a58e6957d98221f39ddc33de27705c14a1b459
amir tadros <amirtadros01@gmail.com>
2024-03-08 12:24:14 -0800

    connect sensor pins to interrupts and declare ISRs

--- SmartCycle/src/main.cpp
+++ SmartCycle/src/main.cpp
@@ -11,12 +11,20 @@ enum class States {
     Shifting
 } current_state;
 
+void reed_switch_isr();
+void shift_up_button_isr();
+void shift_down_button_isr();
+
 void setup() {
   Serial.begin(115200);
 
   pinMode(REED_SWITCH_PIN, INPUT_PULLUP);
   pinMode(SHIFT_UP_BUTTON_PIN, INPUT_PULLUP);
   pinMode(SHIFT_DOWN_BUTTON_PIN, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(REED_SWITCH_PIN), reed_switch_isr, FALLING);
+  attachInterrupt(digitalPinToInterrupt(REED_SWITCH_PIN), shift_up_button_isr, FALLING);
+  attachInterrupt(digitalPinToInterrupt(REED_SWITCH_PIN), shift_down_button_isr, FALLING);
 }
 
 void loop() {
