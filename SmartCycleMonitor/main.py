a19fa580119ec182bdffd89c4d876c78ff22475b
amir tadros <amirtadros01@gmail.com>
2024-03-15 18:38:51 -0700

    cleanup and visual improvements

--- SmartCycleMonitor/main.py
+++ SmartCycleMonitor/main.py
@@ -21,54 +21,63 @@ class SmartCycleMonitor(ctk.CTk):
         # configure 3 row layout
         self.rowconfigure((0, 1, 2), pad=20)
 
+        # Fonts
+        label_font = ctk.CTkFont("ComicSans", 28)
+        text_font = ctk.CTkFont("DS-Digital", 18)
+
         # Title
-        ctk.CTkLabel(self, text="SmartCycle", font=("ComincSans", 42)).grid(row=0, sticky="ew")
+        ctk.CTkLabel(self, text="SmartCycle", font=("ComincSans", 42)).grid(row=0, sticky="EW")
 
         # Meter Frame
-        self.meter_frame = ctk.CTkFrame(self)
-        self.meter_frame.grid(row=1, column=0, sticky="ew")
+        meter_frame = ctk.CTkFrame(self)
+        meter_frame.grid(row=1, column=0, sticky="EW")
 
         # Speedometer Frame
-        self.speedometer_frame = ctk.CTkFrame(self.meter_frame)
-        self.speedometer_frame.grid(row=0, column=0, padx=20, pady=20)
+        speedometer_frame = ctk.CTkFrame(meter_frame)
+        speedometer_frame.grid(row=0, column=0, padx=20, pady=20)
 
         # Speedometer Label
-        ctk.CTkLabel(self.speedometer_frame, text="Speed", font=("ComincSans", 24)).grid(row=0, column=0)
+        ctk.CTkLabel(speedometer_frame, text="Speed", font=label_font).grid(row=0, column=0, pady=5)
 
         # Speedometer Dial
-        self.speedometer = Meter(self.speedometer_frame, border_width=0, fg="#1f6aa5", text_color="white",
+        self.speedometer = Meter(speedometer_frame, border_width=0, fg="#1f6aa5", text_color="white",
                                  text_font="DS-Digital 30", scale_color="white", needle_color="red")
-        self.speedometer.grid(row=1, column=0, padx=20, pady=20)
+        self.speedometer.grid(row=1, column=0, padx=20, pady=5)
 
         # Cadence Frame
-        self.cadence_frame = ctk.CTkFrame(self.meter_frame)
-        self.cadence_frame.grid(row=0, column=1, padx=20, pady=20)
+        cadence_frame = ctk.CTkFrame(meter_frame)
+        cadence_frame.grid(row=0, column=1, padx=20, pady=20)
 
         # Cadence Label
-        ctk.CTkLabel(self.cadence_frame, text="Cadence", font=("ComincSans", 24)).grid(row=0, column=0)
+        ctk.CTkLabel(cadence_frame, text="Cadence", font=label_font).grid(row=0, column=0, pady=5)
 
         # Cadence Dial
-        self.cadence = Meter(self.cadence_frame, border_width=0, fg="#1f6aa5", text_color="white", end=120,
+        self.cadence = Meter(cadence_frame, border_width=0, fg="#1f6aa5", text_color="white", end=120,
                              text_font="DS-Digital 30", scale_color="white", needle_color="red")
-        self.cadence.grid(row=1, column=0, padx=20, pady=20)
+        self.cadence.grid(row=1, column=0, padx=20, pady=5)
 
-        # Gear Frmae
-        self.gear_frame = ctk.CTkFrame(self.meter_frame)
-        self.gear_frame.grid(row=0, column=2, padx=20, pady=20)
+        # Gear Frame
+        gear_frame = ctk.CTkFrame(meter_frame)
+        gear_frame.grid(row=0, column=2, padx=20, pady=5)
 
         # Gear Label
-        ctk.CTkLabel(self.gear_frame, text="Gear", font=("ComincSans", 24)).grid(row=0, column=0)
+        ctk.CTkLabel(gear_frame, text="Gear", font=label_font).grid(row=0, column=0, pady=5)
 
         # Gear Meter
-        self.gear = Meter(self.gear_frame, border_width=0, fg="#1f6aa5", text_color="white", start=1, end=6,
+        self.gear = Meter(gear_frame, border_width=0, fg="#1f6aa5", text_color="white", start=1, end=6,
                           text_font="DS-Digital 30", scale_color="white", needle_color="red")
-        self.gear.grid(row=1, column=0, padx=20, pady=20)
+        self.gear.grid(row=1, column=0, padx=20, pady=5)
 
-        self.raw_data_frame = ctk.CTkFrame(self)
-        self.raw_data_frame.grid(row=2, sticky="ew", padx=20, pady=20)
+        raw_data_frame = ctk.CTkFrame(self)
+        raw_data_frame.grid(row=2, sticky="EW")
+        raw_data_frame.columnconfigure(0, weight=0)
+        raw_data_frame.columnconfigure(1, weight=1)
+        ctk.CTkLabel(raw_data_frame, text="Raw Data: ", font=label_font).grid(row=0, column=0, sticky="E", padx=20)
+
+        raw_data_text_frame = ctk.CTkFrame(raw_data_frame)
+        raw_data_text_frame.grid(row=0, column=1, pady=10, padx=10, sticky="EW")
         self.data_text = ctk.StringVar(value="waiting for data...")
-        data_label = ctk.CTkLabel(self.raw_data_frame, textvariable=self.data_text, font=("ComicSans", 28))
-        data_label.grid(padx=20, pady=20)
+        ctk.CTkLabel(raw_data_text_frame, textvariable=self.data_text, font=text_font).grid(row=0, column=1, pady=20, padx=20)
 
     def connect(self, uri):
         try:
@@ -78,19 +87,18 @@ class SmartCycleMonitor(ctk.CTk):
 
     async def listen(self):
         while True:
-            if self.websocket:
-                if buffer := self.websocket.recv():
-                    self.data_text.set(buffer)
-                    for k, v in json.loads(buffer).items():
-                        match k:
-                            case "speed":
-                                self.speedometer.set(v)
-                            case "cadence":
-                                self.cadence.set(v)
-                            case "gear":
-                                self.gear.set(v)
-                            case _:
-                                print("Unrecognized Data")
+            if self.websocket and (buffer := self.websocket.recv()):
+                self.data_text.set(buffer)
+                for k, v in json.loads(buffer).items():
+                    match k:
+                        case "speed":
+                            self.speedometer.set(v)
+                        case "cadence":
+                            self.cadence.set(v)
+                        case "gear":
+                            self.gear.set(v)
+                        case _:
+                            print("Unrecognized Data")
 
 
 if __name__ == '__main__':
