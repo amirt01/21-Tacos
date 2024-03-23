f69fba7b566308e8725cc418320f81162e80c29c
amir tadros <amirtadros01@gmail.com>
2024-03-22 17:54:15 -0700

    cleanup Button class

--- SmartCycle/include/Button.hpp
+++ SmartCycle/include/Button.hpp
@@ -9,11 +9,12 @@
 
 template<int button_pin>
 class Button {
+  // TODO: measure the optimal debounce_time
   static constexpr auto debounce_time{50};  // [ms]
 
-  byte last_pin_state{HIGH};
-  bool button_state{false};  // false -> released; true -> pressed
-
+  // Pin state tracking variables
+  int last_pin_state{HIGH};
+  enum class ButtonState { Released, Pressed } button_state{};
   unsigned long press_start_time{};
 
  public:
@@ -22,29 +23,34 @@ class Button {
   }
 
   void loop() {
-    const byte pin_state = digitalRead(button_pin);
+    const auto pin_state = digitalRead(button_pin);
 
     if (pin_state == HIGH) {
-      button_state = false;
+      button_state = ButtonState::Released;
+      press_start_time = 0;
     } else if (const auto current_time = millis(); current_time - press_start_time > debounce_time) {
       if (last_pin_state == HIGH) {
         press_start_time = current_time;
       } else {
-        button_state = true;
+        button_state = ButtonState::Pressed;
       }
     }
 
     last_pin_state = pin_state;
   }
 
-  [[nodiscard]] explicit operator bool() const noexcept { return button_state; }
-
-  [[nodiscard]] constexpr bool state() const noexcept { return button_state; }
+  /** Getters **/
+  [[nodiscard]] constexpr bool state() const noexcept { return button_state == ButtonState::Pressed; }
+  [[nodiscard]] explicit operator bool() const noexcept { return this->state(); }
 
   [[nodiscard]] constexpr unsigned long get_press_start_time() const noexcept { return press_start_time; };
 
   [[maybe_unused, nodiscard]] std::string_view to_str() const noexcept {
-    return button_state ? "Pressed" : "Released";
+    switch(button_state) {
+      case ButtonState::Pressed: return "Pressed";
+      case ButtonState::Released: return "Released";
+      default: return "wtf...";
+    }
   }
 };
 
