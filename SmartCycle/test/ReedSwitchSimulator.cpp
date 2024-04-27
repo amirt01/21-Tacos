cc7987f9cafade04a1f852a0d31db2cbfb19ac3f
csimotas <christopher.simotas@gmail.com>
2024-04-27 12:54:06 -0700

    Co-authored-by: amir tadros <amirtadros01@gmail.com>

---
+++ SmartCycle/test/ReedSwitchSimulator.cpp
@@ -0,0 +1,47 @@
+/* 
+* Reed Switch Simulator
+* 
+* Chris Simotas 4/27/2024
+*
+* Send high signals to pin to simulate Reed switch being hit. To be uploaded to seperate board
+*
+*/
+
+#include "Arduino.h"
+#include <esp_timer.h>
+
+// Pulse Timer
+esp_timer_handle_t timer_pulser;
+int sampleRate = 1000000;
+
+void pulser(void *arg){
+    
+    digitalWrite(2, HIGH);
+    delay(50);
+    digitalWrite(2, LOW);
+    
+}
+
+// Create Arguments for Timer
+esp_timer_create_args_t timer_args = {
+  .callback = pulser,
+  .name = "pulser"
+};
+
+
+void setup() {
+    pinMode(2, OUTPUT);
+    
+    // Create Pulser Timer
+    esp_timer_create(&timer_args, &timer_pulser);
+    esp_timer_start_periodic(timer_pulser, sampleRate);
+}
+
+void loop() {
+
+
+    
+
+
+
+}
